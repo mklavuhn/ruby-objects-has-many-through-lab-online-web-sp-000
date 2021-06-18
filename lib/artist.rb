d77f8b5b2891307c286d61b4655a7217cd934dc6
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 19:51:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,28 @@
 class Artist 
-  
-  attr_accessor :name 
-  
+
+  attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
-  def self.all 
-    @@all 
-  end
-  
+
   def songs
     Song.all.select {|song| song.artist == self}
   end
-  
-  def new_song(name, genre)
+
+  def genres
+    songs.map {|song| song.genre}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
     Song.new(name, self, genre)
   end
-  
-  def genres
-    Song.map {|song| song.genre == self}
-  end
-  
-    
-  
-  
+
+
 end
